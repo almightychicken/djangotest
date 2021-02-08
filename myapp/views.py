2c0e0ca6fc15c91d977f7ac46dee302065685ad4
Sviatoslav <jokhiop19@gmail.com>
2021-02-08 19:34:35 +0200

    Gitignore changed

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from .models import TestModel
 
 # Create your views here.
 
 def index(request):
-	return render(request, 'index.html')
+	return render(request, 'index.html', {'titles': TestModel.objects.all()})
